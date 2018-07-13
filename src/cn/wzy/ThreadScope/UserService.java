720cfb1693e55119ddecf8a545b858bf6666536e
1510460325@qq.com <1510460325@qq.com>
2018-07-13 08:43:30 +0800

    提交主函数

--- src/cn/wzy/ThreadScope/UserService.java
+++ src/cn/wzy/ThreadScope/UserService.java
@@ -6,4 +6,7 @@ package cn.wzy.ThreadScope;
  * 不短不长八字刚好
  */
 public class UserService extends Thread {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
